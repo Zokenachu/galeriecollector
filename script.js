73d0fa79b6d5481ed7cbf48c38bc5a20c5dd525f
Zokenachu <89014729+Zokenachu@users.noreply.github.com>
2025-01-15 06:56:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   var password = document.getElementById("password").value;
-  if (password === "yourpassword") {
+  if (password === "GalerieCollectorDeKenzo") {
     document.getElementById("password-screen").style.display = "none";
     document.getElementById("gallery-screen").style.display = "block";
   } else {
